27e9c373d19e66e21d7a5dce0ffbbc939e4352c3
Huy <duyhuy2812@gmail.com>
2026-03-29 22:50:11 +0700

    cập nhật code hiển thị nhân viên với chức vụ

--- app/src/main/java/com/example/btl_mobile_qlns/QuanLyNhanVienActivity.java
+++ app/src/main/java/com/example/btl_mobile_qlns/QuanLyNhanVienActivity.java
@@ -33,7 +33,35 @@ public class QuanLyNhanVienActivity extends AppCompatActivity {
     }
     
     private void taiDanhSachNhanVien() {
-        // Tạm thời hiển thị toast, sẽ làm ListView sau
-        Toast.makeText(this, "Danh sách nhân viên đang được tải...", Toast.LENGTH_SHORT).show();
+        android.util.Log.d("QuanLyNV", "Bắt đầu tải danh sách nhân viên");
+        
+        Cursor cursor = dbHelper.getAllEmployees();
+        android.util.Log.d("QuanLyNV", "Cursor count: " + (cursor != null ? cursor.getCount() : "null"));
+        
+        if (cursor != null && cursor.getCount() > 0) {
+            String[] nhanVienList = new String[cursor.getCount()];
+            int index = 0;
+            
+            while (cursor.moveToNext()) {
+                String maNV = cursor.getString(cursor.getColumnIndexOrThrow("MaNhanVien"));
+                String hoTen = cursor.getString(cursor.getColumnIndexOrThrow("HoTen"));
+                String tenChucVu = cursor.getString(cursor.getColumnIndexOrThrow("TenChucVu"));
+                String tenPhongBan = cursor.getString(cursor.getColumnIndexOrThrow("TenPhongBan"));
+                
+                nhanVienList[index] = maNV + " - " + hoTen + "\n" + tenChucVu + " - " + tenPhongBan;
+                android.util.Log.d("QuanLyNV", "Nhân viên " + index + ": " + nhanVienList[index]);
+                index++;
+            }
+            
+            android.widget.ArrayAdapter<String> adapter = new android.widget.ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, nhanVienList);
+            listView.setAdapter(adapter);
+            
+            cursor.close();
+            Toast.makeText(this, "Đã tải " + index + " nhân viên", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Không có nhân viên nào trong hệ thống", Toast.LENGTH_LONG).show();
+            android.util.Log.d("QuanLyNV", "Không có dữ liệu nhân viên");
+        }
     }
 }
