4f1f24d5202bfa0ca025fab709e68c7308d4342d
Huy <duyhuy2812@gmail.com>
2026-03-29 22:49:52 +0700

    sửa query lấy nhân viên với JOIN

--- app/src/main/java/com/example/btl_mobile_qlns/database/DatabaseHelper.java
+++ app/src/main/java/com/example/btl_mobile_qlns/database/DatabaseHelper.java
@@ -343,9 +343,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
     }
     
-    // Lấy tất cả nhân viên
+    // Lấy tất cả nhân viên với thông tin chức vụ
     public Cursor getAllEmployees() {
         SQLiteDatabase db = this.getReadableDatabase();
-        return db.rawQuery("SELECT * FROM NhanVien", null);
+        String query = "SELECT nv.MaNhanVien, nv.HoTen, nv.GioiTinh, cv.TenChucVu, pb.TenPhongBan " +
+                      "FROM NhanVien nv " +
+                      "LEFT JOIN ChucVu cv ON nv.MaChucVu = cv.MaChucVu " +
+                      "LEFT JOIN PhongBan pb ON nv.MaPhongBan = pb.MaPhongBan " +
+                      "WHERE nv.TrangThaiLamViec = 'Đang làm việc'";
+        return db.rawQuery(query, null);
     }
 }
